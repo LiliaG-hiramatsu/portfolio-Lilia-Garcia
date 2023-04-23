2b4c4742b9cee9554bb134840594800063b7a30b
Lilia <liliagarciahiramatsu@gmail.com>
2023-04-23 11:14:59 -0300

    education and experience components, footer details and site deploy with netlify done

--- src/app/components/educacion/educacion.component.ts
+++ src/app/components/educacion/educacion.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent {
+export class EducacionComponent implements OnInit {
 
+  educacionList:any;
+  cursosList:any;
+
+  constructor(private datosPortfolio: PortfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.educacionList = data.educacion;
+    });
+
+    this.datosPortfolio.obtenerDatos().subscribe(data => {
+      this.cursosList = data.cursos;
+    });
+  }
 }
